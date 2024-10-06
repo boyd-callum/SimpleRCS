6f99964d3efaabbc810fa9a947c79a80fe181a04
Callum Boyd <cboy0015@student.monash.edu>
2024-10-07 01:04:01 +1100

    Improve data saving code

--- Matlab Code/SDdataAnalysis.m
+++ Matlab Code/SDdataAnalysis.m
@@ -1,12 +1,18 @@
 clear all; close all; clc;
 
+[file, path] = uigetfile('*.csv');
 
-sinedata = importdata("EXAMPLE.CSV").data();
-time = sinedata(:,1)/ 1000.0; % in seconds
-values = sinedata(:,2);
+sensorData = importdata(fullfile(path, file)).data();
+time = sensorData(:,1)/ 1000.0; % in seconds
+thetadot = sensorData(:,2);
+throttle = sensorData(:,3);
 
-plot(time, values, '.')
+plot(time, [thetadot,throttle], '-')
+hold on
+yline(0)
 title('Example Data')
 xlabel('Time (s)')
-ylabel('Amplitude')
+ylabel('Theta Dot (deg/s)')
+legend("Theta Dot", "Throttle")
+
 
--- Python Code/save_sensor_data.py
+++ Python Code/save_sensor_data.py
@@ -2,16 +2,16 @@ import serial
 import time
 
 # Configure the serial port
-serial_port = 'COM3'  # Update with your port (e.g., COM3 for Windows, /dev/ttyUSB0 for Linux)
-baud_rate = 57600  # Match the baud rate with your Arduino
-output_file = 'headingData6.csv'
+serial_port = '/dev/cu.usbserial-140'  # 
+baud_rate = 9600  # Match the baud rate with the arduino
+output_file = 'throttledata1.csv'
 
 # Open the serial port
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
 time.sleep(2)  # Give some time for the serial connection to establish
 
 line_count = 0
-wanted_line_count = 5000  # Number of lines to read
+wanted_line_count = 500  # Number of lines to read
 
 # Open the text file
 with open(output_file, mode='w') as file:
